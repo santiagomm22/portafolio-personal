09d02939ea2067bd02b7377b85ce6e0f1a6db1dc
santiagomm22 <sntgmm22@gmail.com>
2026-01-05 21:50:47 -0500

    correccion de tama;o de texto

--- src/components/hero-inicial/HeroText.tsx
+++ src/components/hero-inicial/HeroText.tsx
@@ -6,15 +6,15 @@ const HeroText = () => {
       {/* Contenedor para el título y subtítulo centrados */}
       <div className="flex-grow flex flex-col justify-center items-center text-center">
         {/* Título principal en grande */}
-        <h1 className="text-6xl md:text-8xl font-bold tracking-wider mb-6 uppercase text-white">
+        <h1 className="text-5xl sm:text-5xl md:text-6xl lg:text-8xl font-bold tracking-wider mb-6 uppercase text-white">
           SANTIAGO MENDOZA
         </h1>
 
         {/* Subtítulo */}
-        <p className="text-lg md:text-xl uppercase tracking-widest font-medium text-gray-300">
+        <p className="text-sm sm:text-base md:text-lg lg:text-xl uppercase tracking-widest font-medium text-gray-300">
           <DecryptedText
             text="DESARROLLADOR FRONT-END Y BACK-END"
-            className="text-lg md:text-xl uppercase tracking-widest font-medium text-gray-300"
+            className="text-sm sm:text-base md:text-lg lg:text-xl uppercase tracking-widest font-medium text-gray-300"
             animateOn="view"
             speed={80}
             maxIterations={15}
